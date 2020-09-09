402b3af104d43525958b045a1d6a5d35d99e9823
ranisup97 <51501974+ranisup97@users.noreply.github.com>
2020-09-10 00:56:28 +0700

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,26 @@
+const biodata = {
+    name: "Rani Suprianti",
+    age: 23,
+    hobbies: ["gaming","hiking","cooking"],
+    isMarried: false,
+    schoolList: [
+        {namaSekolah: "SDN Sukajaya V", yearIn: 2003, yearOut: 2009, major: "null"},
+        {namaSekolah: "SMPN 1 Tarogong Kidul", yearIn: 2009, yearOut: 2012, major: "null"},
+        {namaSekolah: "SMAN 11 Garut", yearIn: 2012, yearOut: 2015, major: "IPA"},
+        {namaSekolah: "Universitas Pendidikan Indonesia", yearIn: 2015, yearOut: 2020, major: "Ilmu Komputer"}
+    ],
+    skills: [
+        {skillName: "C", level: "beginner"},
+        {skillName: "CodeIgniter", level: "beginner"},
+        {skillName: "HTML", level: "beginner"},
+    ],
+    interestInCoding: true
+}
+
+// console.log(biodata.name)
+// console.log(biodata.age)
+// console.log(biodata.hobbies)
+// console.log(biodata.isMarried)
+// console.log(biodata.schoolList)
+// console.log(biodata.skills)
+// console.log(biodata.interestInCoding)
